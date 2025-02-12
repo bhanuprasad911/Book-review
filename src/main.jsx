5a74ae90cbbaa7857d343f384c2513fb759cf55e
bhanuprasad911 <bhanuprasadchintu.170403@gmail.com>
2025-02-12 17:07:50 +0530

    h

--- src/main.jsx
+++ src/main.jsx
@@ -13,11 +13,11 @@ createRoot(document.getElementById('root')).render(
     <ThemeProvider>
       <AuthProvider>
         <BookContextProvider>
-        <BooklistProvider>
           <Router>
+        <BooklistProvider>
             <App />
-          </Router>
         </BooklistProvider>
+          </Router>
         </BookContextProvider>
       </AuthProvider>
     </ThemeProvider>
